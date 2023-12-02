4f3373241a33cceaa7417c0fca5b827b29252a3f
IndusAryan <serena01011975@gmail.com>
2023-12-03 00:04:37 +0530

    add insane logic and flexes

--- script/game.js
+++ script/game.js
@@ -22,34 +22,75 @@ function playGame(playerMove) {
     const cpusMove = cpuMove();
 
     var result = '';
+    var moveofcpuImage = document.querySelector('.moveofcpu');
+    var moveofplayerImage = document.querySelector('.moveofplayer');
 
     if (playerMove === 'scissors') {
         if (cpusMove === 'rock') {
-            result = ('Loser! Get TF outta here 👻')
+            result = ('Looser! 👻');
+            moveofcpuImage.src = 'assets/rock.png';
         } else if (cpusMove === 'paper') {
-            result = ('You Win! 👑')
+            result = ('You Win! 👑');
+            moveofcpuImage.src = 'assets/paper.png';
         } else if (cpusMove === 'scissors') {
-            result = ('Game Tied 💀')
+            result = ('Game Tied 💀');
+            moveofcpuImage.src = 'assets/scissors.png';
         }
+        moveofplayerImage.src = 'assets/scissors.png';
 
     } else if (playerMove === 'paper') {
         if (cpusMove === 'rock') {
-            result = ('You Win! 👑')
+            result = ('You Win! 👑');
+            moveofcpuImage.src = 'assets/rock.png';
         } else if (cpusMove === 'paper') {
-            result = ('Game Tied 💀')
+            result = ('Game Tied 💀');
+            moveofcpuImage.src = 'assets/paper.png';
         } else if (cpusMove === 'scissors') {
-            result = ('Loser! Get TF outta here 👻')
+            result = ('Looser! 👻');
+            moveofcpuImage.src = 'assets/scissors.png';
         }
+        moveofplayerImage.src = 'assets/paper.png';
 
     } else if (playerMove === 'rock') {
         if (cpusMove === 'rock') {
-            result = ('Game Tied 💀')
+            result = ('Game Tied 💀');
+            moveofcpuImage.src = 'assets/rock.png';
         } else if (cpusMove === 'paper') {
-            result = ('Loser! Get TF outta here 👻')
+            result = ('Looser! 👻');
+            moveofcpuImage.src = 'assets/paper.png';
         } else if (cpusMove === 'scissors') {
-            result = ('You Win! 👑')
+            result = ('You Win! 👑');
+            moveofcpuImage.src = 'assets/scissors.png';
         }
+        moveofplayerImage.src = 'assets/rock.png';
     }
 
     document.querySelector('.resultStatus').innerHTML = result;
+
+    if (result === 'You Win! 👑') {
+        score.wins += 1;
+    } else if (result === 'Looser! 👻') {
+        score.losses += 1;
+    } else if (result === 'Game Tied 💀') {
+        score.ties += 1;
+    }
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement();
+
+    document.querySelector('.result').innerHTML = result;
 }
+
+function updateScoreElement() {
+    document.querySelector('.stats')
+        .innerHTML = `Victories: ${score.wins} | Defeats: ${score.losses} | Ties: ${score.ties}`;
+}
+
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+};
+
+updateScoreElement();
