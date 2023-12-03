1025ce174819be40d48465a686e676984cb12e6e
IndusAryan <serena01011975@gmail.com>
2023-12-03 18:37:04 +0530

    optimize and add intervals

--- script/soundEffects.js
+++ script/soundEffects.js
@@ -1,34 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('.theme, .resetScore, .scissors-button, .paper-button, .rock-button');
 
-    var xButton = document.querySelector('.theme');
-    var yButton = document.querySelector('.resetScore');
-    var zButton = document.querySelector('.scissors-button');
-    var pButton = document.querySelector('.paper-button');
-    var rButton = document.querySelector('.rock-button');
-
-    function playSoundMove3() {
-        var audio = new Audio('assets/sword.wav');
+    function playSound(sound) {
+        var audio = new Audio(`assets/${sound}.wav`);
         audio.play();
     }
 
-    function playSoundMove2() {
-        var audio = new Audio('assets/paper.wav');
-        audio.play();
-    }
-
-    function playSoundMove1() {
-        var audio = new Audio('assets/rock.wav');
-        audio.play();
-    }
-
-    function playSound() {
-        var audio = new Audio('assets/themeChange.wav');
-        audio.play();
-    }
-
-    xButton.addEventListener('click', playSound);
-    yButton.addEventListener('click', playSound);
-    zButton.addEventListener('click', playSoundMove3);
-    pButton.addEventListener('click', playSoundMove2);
-    rButton.addEventListener('click', playSoundMove1);
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var sound = button.dataset.sound;
+            playSound(sound);
+        });
+    });
 });
